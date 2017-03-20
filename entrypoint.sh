a54c293d7e08513d5d9519ddf84b6e6d406094dc
Shaun Smiley <shaun.smiley@mtnsat.com>
2017-03-20 13:46:10 -0700

    fix OPT_PARAMS for sh (not bash)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 #/usr/sbin/named -c /etc/bind/named.conf -g -u named
 
 
-OPT_PARAMS='-c ' 
-OPT_PARAMS+='/etc/bind/named.conf '
-OPT_PARAMS+='-g '
-OPT_PARAMS+='-u root '
+OPT_PARAMS='-c' 
+OPT_PARAMS="$OPT_PARAMS /etc/bind/named.conf"
+OPT_PARAMS="$OPT_PARAMS -g"
+OPT_PARAMS="$OPT_PARAMS -u root"
 
 
 disable_ipv6="${DISABLE_IPV6:-True}"
 
 if [ "$disable_ipv6" == True ]; then
-  OPT_PARAMS+="-4 "
+  OPT_PARAMS="$OPT_PARAMS -4"
 fi
 
 /usr/sbin/named "${OPT_PARAMS}"
